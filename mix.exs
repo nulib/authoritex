b2f6ac6a55ad0d09910368599785e1842b4efcac
Michael B. Klein <mbklein@gmail.com>
2020-05-11 12:17:03 -0500

    Add circleci/coveralls config

--- mix.exs
+++ mix.exs
@@ -5,9 +5,17 @@ defmodule Authoritex.MixProject do
     [
       app: :authoritex,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.circle": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,6 +29,7 @@ defmodule Authoritex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:excoveralls, "~> 0.12.3", only: [:dev, :test]},
       {:httpoison, "~> 1.6.2"},
       {:sweet_xml, "~> 0.6"}
     ]
